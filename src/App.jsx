7ac00c39d2e7d90dd36dc2b7fb556ef9e1cbf8dc
FrancoChSa28 <a20173126@pucp.edu.pe>
2024-09-17 08:56:03 -0500

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-6xl text-white text-opacity-50'>Vite + React</h1>
+      <h1 className='text-6xl text-white text-opacity-50'>Vite + React + Git deployment</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
